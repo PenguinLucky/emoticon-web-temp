b415c55966d32c9351c59f29ee484e683033b6c0
PenguinLucky <45398456+PenguinLucky@users.noreply.github.com>
2020-05-10 19:55:01 +0900

    emoticon-bot.f5.si

--- assets/js/anchor.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  $('a[href^="#"]').click(function(){
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    $("html, body").animate({ scrollTop : target.offset().top }, 800 );
-  });
-});
